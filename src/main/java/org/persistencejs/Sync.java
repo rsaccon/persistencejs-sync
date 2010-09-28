dce5a615c267fe1726bbe818559ce05b479541cf
Roberto Saccon <rsaccon@gmail.com>
2010-09-28 16:04:48 -0300

    initial commit

---
+++ src/main/java/org/persistencejs/Sync.java
@@ -0,0 +1,16 @@
+package org.persistencejs;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Sync {
+
+    boolean enabled() default false;
+    boolean timestamp() default false;
+}
